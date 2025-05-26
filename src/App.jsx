e02dabae624cd0d2a399ca065d344d5ad0e16a3f
kiranmr2024 <kiranmr2024@gmail.com>
2025-05-26 14:47:12 +0000

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import './App.css';
+import React from 'react';
+import Ques_2 from './Module 3/Introduction to ReactJS/Ques_2';
+
 
 function App() {
-
-  return (
-    <>
-      {/* Add your component here */}
-    </>
-  )
+   return (
+    <div>
+      <h1>ReactJS Question 2</h1>
+      <Ques_2 />
+    </div>
+  );
 }
+export default App;
 
-export default App
